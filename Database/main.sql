5ff0718e9d74a6bfc4afa0d36139ef0a4c19abc9
jellyfishcxdding <gracia.valerine54@sma.belajar.id>
2026-09-08 21:36:15 +0700

    modified sql

--- Database/main.sql
+++ Database/main.sql
@@ -1,4 +1,7 @@
-Create Table{
-    name: string;
-    
-}
+CREATE TABLE medication_logs (
+    id SERIAL PRIMARY KEY,
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    elder_name TEXT NOT NULL,
+    medication_status TEXT NOT NULL,
+    notes TEXT
+);
